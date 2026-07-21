e676bd7a1fbd3649c0cbfc975e9b97a2448867a3
Jonny Candes <arjscandes73@gmail.com>
2026-07-21 18:21:39 +0800

    Delete

--- scripts/make_migration.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Generate a new Alembic migration."""
-import subprocess
-import sys
-
-if __name__ == "__main__":
-    message = sys.argv[1] if len(sys.argv) > 1 else "auto migration"
-    subprocess.run(["alembic", "revision", "--autogenerate", "-m", message], check=True)
